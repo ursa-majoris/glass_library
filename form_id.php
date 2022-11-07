6b1414a654235341e2c31484a6949444c0c5f44d
ursa-majoris <96943472+ursa-majoris@users.noreply.github.com>
2022-11-07 10:30:52 +0600

    Update form_id.php

--- form_id.php
+++ form_id.php
@@ -6,18 +6,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $obj_id = $_POST["obj_id"];
     //$_SESSION["id"] = $id;
 
-    $db_host='localhost'; // ваш хост
-    $db_name='glass_lib'; // ваша бд
-    $db_user='root'; // пользователь бд
-    $db_pass='liberty'; // пароль к бд
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+//    $db_host='localhost'; // ваш хост
+//    $db_name='glass_lib'; // ваша бд
+//    $db_user='user1'; // пользователь бд
+//    $db_pass='Saule'; // пароль к бд
+
+    $db_host='db.fai.kz'; // ваш хост
+    $db_name='votest'; // ваша бд
+    $db_user='vo'; // пользователь бд
+    $db_pass='LongLiveVO!'; // пароль к бд
+
+
+    //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-    $mysqli->set_charset("utf8_general_ci");
+    //echo "$mysqli";
+    //$mysqli->set_charset("utf8_general_ci");
     $link = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
     if (!$link) {
-        echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+        echo 'Can not connect with database: ' . mysqli_connect_errno() . ', error: ' . mysqli_connect_error();
+
+        if(isset($_SERVER['HTTP_REFERER'])) {
+            $urlback = htmlspecialchars($_SERVER['HTTP_REFERER']);
+            echo "<br>";
+            echo "<a href='$urlback' class='history-back'> Go back </a><br>";
+        }
+
         exit;
     }
+    $search_rez_bool = false;
     $sql = mysqli_query($link, 'SELECT *  FROM `glass_database`');
     while ($result = mysqli_fetch_array($sql)) {
         if($result['id'] == $obj_id){
@@ -34,12 +50,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "Observer: ".$result['observer']. "<br>";
             echo "FITS: ".$result['url']. "<br>";
             echo "<br>";
+            $search_rez_bool = true;
         }
 
+        //echo "itteraton <br>";
 
 
 
+    }
+    if (!$search_rez_bool){
+        echo "Unfortunately there is no astroplate with such id <br>";
+        if(isset($_SERVER['HTTP_REFERER'])) {
+            $urlback = htmlspecialchars($_SERVER['HTTP_REFERER']);
+            echo "<br>";
+            echo "<a href='$urlback' class='history-back'> Go back </a><br>";
+        }
+        exit();
 
+
+    }
+
+    if(isset($_SERVER['HTTP_REFERER'])) {
+        $urlback = htmlspecialchars($_SERVER['HTTP_REFERER']);
+        echo "<br>";
+        echo "<a href='$urlback' class='history-back'> Go back </a><br>";
     }
 
 
